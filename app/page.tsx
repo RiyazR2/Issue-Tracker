fb7959b43064f4d585f4a3258264504e9f02267e
Riyaz Pathan <riyazpathan193.rp@gmail.com>
2025-04-06 12:21:56 +0530

    netlify update 3

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,24 @@ import LatestIssues from "./LatestIssues";
 import { Metadata } from "next";
 
 export default async function Home() {
-  const open = await prisma.issue.count({ where: { status: "OPEN" } });
-  const inProgress = await prisma.issue.count({
-    where: { status: "IN_PROGRESS" },
-  });
-  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
-  console.log("NEXTAUTH_URL:", process.env.NEXTAUTH_URL);
-  console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
+  // const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  // const inProgress = await prisma.issue.count({
+  //   where: { status: "IN_PROGRESS" },
+  // });
+  // const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+  // console.log("NEXTAUTH_URL:", process.env.NEXTAUTH_URL);
+  // console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
+  let open = 0,
+    inProgress = 0,
+    closed = 0;
 
+  try {
+    open = await prisma.issue.count({ where: { status: "OPEN" } });
+    inProgress = await prisma.issue.count({ where: { status: "IN_PROGRESS" } });
+    closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+  } catch (err) {
+    console.error("Prisma error:", err);
+  }
   return (
     <Grid columns={{ initial: "1", md: "2" }} gap="5">
       <Flex direction="column" gap="5">
